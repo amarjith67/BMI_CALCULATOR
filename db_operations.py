aeb0c0a58fd8b971e6da47f7cbaa9ebad7be36d0
Amar1013 <amarjithraj67@gmail.com>
2022-03-03 20:36:54 +0530

    final commit

---
+++ db_operations.py
@@ -0,0 +1,34 @@
+import cx_Oracle 
+
+class bmi_db:
+    def __init__(self,username,pass_word):
+        self.conn=cx_Oracle.connect(user=username,password=pass_word,dsn="localhost/xe")
+        self.cursor=self.conn.cursor()
+        print("connected")
+        
+    def insert_data(self,user_id,name,age,height,weight):
+        self.cursor.execute(f"INSERT INTO BMI VALUES({user_id},'{name}',{age},{height},{weight},NULL)")  
+        self.bmi=(float(weight)/(float(height)*float(height)))*10000
+        self.cursor.execute(f"UPDATE BMI SET BMI_VAL={self.bmi}")
+        if(self.bmi<18.5):
+            self.message="Underweight"
+        elif self.bmi >= 18.5 and self.bmi <= 24.9 :
+             self.message="Normal"
+        elif self.bmi >= 25.0 and self.bmi <= 29.9 :
+             self.message="OverWeight" 
+        elif self.bmi >= 30:
+           self.message="Obese" 
+        self.conn.commit()
+        return self.bmi,self.message 
+    
+    def view_data(self,user_id,name):
+        self.data=self.cursor.execute(f"SELECT * FROM BMI WHERE ID={user_id} AND NAME='{name}'")    
+        self.conn.commit()
+        return self.cursor,self.bmi
+    
+    def update_data(self,user_id,name,age,height,weight):
+        self.bmi=(float(weight)/(float(height)*float(height)))*10000
+        self.cursor.execute(f"UPDATE BMI SET NAME='{name}',AGE={age},HEIGHT={height},WEIGHT={weight},BMI_VAL={self.bmi} WHERE  ID={user_id}")
+        self.conn.commit()
+        return self.cursor
+        
---
+++ index.py
@@ -0,0 +1,56 @@
+
+from email import message
+from flask import Flask,render_template,request, url_for
+from db_operations import bmi_db
+import cx_Oracle
+
+app=Flask(__name__)
+db=bmi_db('amar','amar')
+
+@app.route("/")
+def landing():
+    return render_template("index.html");
+
+@app.route("/home")
+def home():
+    return render_template("index.html");
+
+
+@app.route("/send", methods = ['GET', 'POST'])
+def send():
+    if(request.method=='POST'):
+        user_id = request.form.get('id')
+        name = request.form.get('name')
+        age = request.form.get('age')
+        height = request.form.get('height')
+        weight = request.form.get('weight')
+        data,message=db.insert_data(user_id,name,age,height,weight)      
+    return render_template('insert.html',data=data,message=message)
+
+@app.route("/existing_user")
+def existing_user():
+    return render_template('existing_user.html')
+
+@app.route("/view_user",methods = ['GET', 'POST'])
+def view():
+    if(request.method=='POST'):
+        user_id = request.form.get('id')
+        name = request.form.get('name')
+        data,bmi=db.view_data(user_id,name)
+    return render_template('user_view.html',data=data,bmi=bmi)
+
+@app.route("/update_user")
+def update_user():
+    return render_template('update.html')
+    
+@app.route("/send_update", methods = ['GET', 'POST'])
+def send_update():
+     if(request.method=='POST'):
+        user_id = request.form.get('id')
+        name = request.form.get('name')
+        age = request.form.get('age')
+        height = request.form.get('height')
+        weight = request.form.get('weight')
+        db.update_data(user_id,name,age,height,weight)
+     return render_template('success.html')    
+app.run(debug=True)
